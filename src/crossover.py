74be1012d0e2cab2ff9d383f6d98c832b1653717
gleydsonbrito <gleydsonbrito@hotmail.com>
2021-04-16 13:40:49 -0300

    add file: crossover, a basic strategy for crossover individuals

---
+++ src/crossover.py
@@ -0,0 +1,11 @@
+import random
+
+def mate(indA, indB):
+    iA = indA.copy()
+    iB = indB.copy()
+    q_points = random.randint(0, 29)
+    for i in range(q_points):
+        point = random.randint(0, 29)
+        iA[point], iB[point] = iB[point], iA[point]
+
+    return iA, iB
--- src/toolbox.py
+++ src/toolbox.py
@@ -1,6 +1,7 @@
 from deap import base, tools, creator
 from evaluate import evaluate
 from individual import create_individual
+from crossover import mate
 
 creator.create('FitnessMin', base.Fitness, weights=(-1.0,))
 creator.create('Individual', list, fitness=creator.FitnessMin)
@@ -12,7 +13,7 @@ def get_toolbox():
                     creator.Individual, toolbox.cromossome)
     toolbox.register('population', tools.initRepeat, list, toolbox.individual)
     toolbox.register('select', tools.selTournament, tournsize=3)
-    toolbox.register('mate', tools.cxOrdered)
+    toolbox.register('mate', mate)
     toolbox.register('mutate', tools.mutShuffleIndexes, indpb=0.05)
     toolbox.register('evaluate', evaluate)
 
