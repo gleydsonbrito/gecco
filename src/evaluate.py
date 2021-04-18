5327b875c2228abf0fbdddb9bbd3ffd6b1687426
gleydsonbrito <gleydsonbrito@hotmail.com>
2021-04-18 13:52:31 -0300

    create test_eval file for test evaluation of individuals, and fix imports in evaluate file

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,10 +1,10 @@
-import random
 import subprocess
 
 
 def evaluate(candidateSolution):
     evalCommand = "docker run --rm mrebolle/r-geccoc:Track1 -c 'Rscript objfun.R "
     parsedCandidate = ",".join([str(x) for x in candidateSolution])
-    evall = (subprocess.check_output(evalCommand + '"' + parsedCandidate + '"\'', shell=True))
+    evall = (subprocess.check_output(evalCommand +
+                                     '"' + parsedCandidate + '"\'', shell=True))
     res = float(evall)
-    return res, 
+    return res,
