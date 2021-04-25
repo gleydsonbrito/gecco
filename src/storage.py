fceda6a661dde618aee12abda67684136f0154f9
gleydsonbrito <gleydsonbrito@hotmail.com>
2021-04-25 13:15:00 -0300

    fix get_csv function

--- src/storage.py
+++ src/storage.py
@@ -9,6 +9,17 @@ def save_individual(fit, cromossome):
 
 
 def get_csv():
-    data = pd.read_csv('../files/individuals.csv')
-    inds = data.values.tolist()
+    inds = []
+    data_frame = pd.read_csv('files/individuals.csv')
+    for i in data_frame.values.tolist():
+        float_croms = []
+        fit = float(i[0][1:7])
+        final_pos = len(i[1])-1
+        substring_croms = i[1][1:final_pos]
+        string_croms = substring_croms.split(sep=',')
+        for c in string_croms:
+            float_croms.append(float(c.strip()))
+
+        inds.append([fit, float_croms])
+
     return inds
