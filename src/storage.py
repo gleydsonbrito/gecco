f59459e1e849c6c21d481320ae7a59f51bd80bc7
gleydsonbrito <gleydsonbrito@hotmail.com>
2021-04-25 22:19:26 -0300

    refactor storage file and rename variables

--- src/storage.py
+++ src/storage.py
@@ -14,9 +14,8 @@ def get_csv():
     for i in data_frame.values.tolist():
         float_croms = []
         fitness = float(i[0][1:-2])
-        final_pos = len(i[1])-1
-        substring_croms = i[1][1:final_pos]
-        string_croms = substring_croms.split(sep=',')
+        extracted_croms = i[1][1:-1]
+        string_croms = extracted_croms.split(sep=',')
         for c in string_croms:
             float_croms.append(float(c.strip()))
 
