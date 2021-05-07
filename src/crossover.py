a65bb5bdc027f4cd0b1bd43221d3387dd9a952c7
gleydsonbrito <gleydsonbrito@hotmail.com>
2021-05-07 20:15:24 -0300

    fix mate function= create indviduals copy

--- src/crossover.py
+++ src/crossover.py
@@ -1,10 +1,16 @@
 import random
 from storage import save_individual
+from deap import base
 
 
 def mate(indA, indB):
+    copy_a = base.Toolbox().clone(indA)
+    copy_b = base.Toolbox().clone(indB)
+
     q_points = random.randint(0, 28)
+
     for i in range(q_points):
-        point = random.randint(0, 28)
-        indA[point], indB[point] = indB[point], indA[point]
-    return indA, indB
+        gene_position = random.randint(0, 28)
+        copy_a[gene_position], copy_b[gene_position] = copy_b[gene_position], copy_a[gene_position]
+
+    return copy_a, copy_b
--- src/mutate.py
+++ src/mutate.py
@@ -1,4 +1,5 @@
 from constants import LOWER, UPPER
+from deap import base
 from storage import get_csv
 from sklearn.cluster import KMeans
 from scipy.spatial import distance
@@ -12,6 +13,7 @@ def mutate(ind):
 
 
 def centroid_mutate(ind):
+    ind_clone = base.Toolbox().clone(ind)
     cromossomes = [c[1] for c in get_csv()]
 
     kmeans = KMeans(n_clusters=6, init="k-means++")
@@ -19,21 +21,22 @@ def centroid_mutate(ind):
 
     centroids = kmeans.cluster_centers_
     min_distance = float('inf')
-    mutated_ind = []
+    mutate_ind = []
 
     for i in range(len(centroids)):
-        dist = distance.euclidean(ind, centroids[i])
+        dist = distance.euclidean(ind_clone, centroids[i])
         if dist < min_distance:
             min_distance = dist
             mutated_ind = centroids[i]
 
     for i in range(len(mutated_ind)):
-        ind[i] = mutated_ind[i]
+        ind_clone[i] = mutated_ind[i]
 
-    return ind,
+    return ind_clone,
 
 
 # mut = centroid_mutate([6.815736838193935, 11.66095896456695, 5.890178907164971, 3.7102530295496177, 6.070594996464887, 7.403798774843138, 3.694358678086288, 3.56187077108757, 34.566743841688805, 23.660429548127897, 3.6600788353571154, 6.815460134330752, 1.8776378942214158, 0.10704545213839409,
 #                        0.09501376922236714, 0.01666207407361643, 0.12284435086362626, 0.001969933160091152, 0.08383424707133179, 0.3482460098670257, 0.11807448413129469, 0.6401878630861229, 0.003723697952887368, 3.7731433506168774, 0.9124103412691668, 0.06116595456316067, 1.259443125795108, 2.775846595652113, 0.69744733900394])
 
 # print(mut)
+# print(mut.fitness.value)
